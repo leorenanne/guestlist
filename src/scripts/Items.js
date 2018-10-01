7c84a9c7b22fef597cbaf5f481a83d35d86c3568
Angus McInnes <angus@amcinnes.info>
2018-10-01 22:10:14 +1000

    Give items an id
    Using index as key can be problematic (
    https://medium.com/@robinpokorny/index-as-a-key-is-an-anti-pattern-e0349aece318
    ). Using an id will enable React to properly handle reordering/adding/removing
    items from the list (which we'll probably want to do at some point).

--- src/scripts/Items.js
+++ src/scripts/Items.js
@@ -7,9 +7,9 @@ class Items extends React.Component {
     return (
       <div>
         <Grid container spacing={24}> 
-        {this.props.items.map(function(item, index){
+        {this.props.items.map(function(item){
           return (
-            <Grid item xs={6} sm={3} key={index}>
+            <Grid item xs={6} sm={3} key={item.id}>
               <Item item={item} />
             </Grid>
           );
